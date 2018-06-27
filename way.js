62b5b2df2bcca9c933676b7442e670efd25d0e5d
Michael Allan <mike@reluk.ca>
2018-06-27 14:52:29 -0400

    Purpose, method and formatting of personal configuration files.

--- way.js
+++ way.js
@@ -1,43 +1,82 @@
-/** way.js - My personal configuration script for way declaration documents (way.xht)
+/** way.js - Personal configuration program for my way declaration documents
   *
-  *   See also § Personalized configuration § way.js: http://reluk.ca/project/wayic/cast/doc.task
+  *   This program runs once for each way declaration document (way.xht) in my waycast.
+  *   Together with the style sheet way.css, it configures the web view of the document.
+  *   Details: http://reluk.ca/project/wayic/cast/doc.task § Personal configuration
   */
 'use strict';
 ( function()
 {
 
 
-    /** Appends a *script* element to the document *body*.
+    /** Runs this program, configuring the view of the document.
       *
-      *     @param src (string) The value of the *src* attribute.
+      *     @param dirWayic (string) Location of the wayic projects directory in URL form,
+      *       with a trailing slash '/'.
       */
-    function appendScript( src )
+    function run( dirWayic )
     {
-        const s = document.createElementNS( NS_HTML, 'script' );
-        s.setAttribute( 'src', src );
-        document.body.appendChild( s );
+      // Make the document readable on the web, transforming it with wayic.read.readable
+      // -------------------------------------
+        loadProgram( dirWayic + 'read/readable.js', ( _Event ) =>
+        {
+            let wasRequestFileSchemed = false;
+            let wasRequestViaLocalWeb = false;
+            const docLoc = document.URL;
+            if( docLoc.startsWith( 'file:' )) wasRequestFileSchemed = true;
+            else if( docLoc.startsWith( 'http://obsidian/' )) wasRequestViaLocalWeb = true;
+
+          // ? With constraints enforced
+          // ---------------------------
+            wayic.read.readable.setEnforceConstraints(
+                // true                  // Yes
+                   wasRequestFileSchemed // Only for me, testing locally via the file system
+                // wasRequestViaLocalWeb // Only for me, testing locally via the web server
+                // false                 // No
+              );
+
+          // ? With dark lighting for my Chrome test browser
+          // --------------------
+            const toDarkenChrome =
+                // true                  // Yes
+                // wasRequestFileSchemed // Only for me, testing locally via the file system
+                // wasRequestViaLocalWeb // Only for me, testing locally via the web server
+                   false                 // No
+              ;
+            if( toDarkenChrome ) wayic.read.readable.setLightingStyle( 'neon' );
+              // Till I learn how to darken Chrome more directly, e.g. by its own settings
+
+          // Start the transformer
+          // ---------------------
+            wayic.read.readable.start();
+        });
     }
 
 
 
-    /** Configures the present document.
+/// ====================================================================================================
+
+
+    /** Requests that another program load.
+      *
+      *     @param loc (string) Location of the program in URL form.
+      *     @param callback (Function) What to call when the program loads, or null to call nothing.
       */
-    function configure()
+    function loadProgram( loc, callback = null )
     {
-        const dirWP = 'http://reluk.ca/project/wayic/'; // wayic projects directory
-        appendScript( dirWP + 'read/readable.js' );
+        const s = document.body.appendChild(
+          document.createElementNS( 'http://www.w3.org/1999/xhtml', 'script' ));
+        s.setAttribute( 'src', loc );
+        if( callback !== null ) s.addEventListener( 'load', callback );
     }
 
 
 
-    /** The XML namespace of HTML.
-      */
-    const NS_HTML = 'http://www.w3.org/1999/xhtml';
+/// ================
 
-
-
-////////////////////
-
-    configure();
+    run(
+      'http://reluk.ca/project/wayic/'
+   // '/home/mike/var/deploy/wayic/test/' // TEST purposes only
+      );
 
 }() );
